0956ef258cd281ba767c94024c467720d077417f
Thomas AUGUEY <dev@aceteam.org>
2012-11-21 22:05:11 +0100

    samples views update

---
+++ samples/js/form.js
@@ -0,0 +1,67 @@
+/*
+(C)2012 AceTeaM. WebFrameWork(R) All rights reserved.
+---------------------------------------------------------------------------------------------------------------------------------------
+Warning this script is protected by copyright, if you want to use this code you must ask permission:
+Attention ce script est protege part des droits d'auteur, si vous souhaitez utiliser ce code vous devez en demander la permission:
+Auteur : AUGUEY THOMAS
+Mail   : dev@aceteam.org
+---------------------------------------------------------------------------------------------------------------------------------------
+
+Script lié au document "navigator.html"
+
+Implentation: [18-10-2012]
+*/
+
+//initialise le contenu
+YUI(wfw_yui_config(wfw_yui_base_path)).use('node', 'wfw-navigator', 'wfw-fieldbar', function (Y)
+{
+    var wfw = Y.namespace("wfw");
+    
+    var onLoad = function(e){
+        
+        /*
+        * ---------------------------------------------------------------
+        * Affiche l'id du fichier
+        * ---------------------------------------------------------------
+        */
+        Y.Node.one("#page_id").set("text", wfw.Navigator.pageId);
+
+        /*
+        * ---------------------------------------------------------------
+        * Initialise la barre de navigation
+        * ---------------------------------------------------------------
+        */
+        wfw.FieldBar.initElement(Y.one("#nav_input"), {
+            //FIELD_BAR options
+            barClass: "fieldbar_fil_ariane_bar wfw_bg_frame",
+            itemClass: "fieldbar_fil_ariane_item",
+            itemEvent: "fieldbar_fil_ariane",
+            editable: false,
+            contener: Y.Node.one("#nav_bar"),
+            //Events
+            onCreateItem: function(item,label){
+                //insert une icon aux items
+                var icon = Y.Node.create('<span>');
+                icon.addClass('fieldbar_fil_ariane_item_image');
+                label.insert(icon,'before');
+                
+		//onClick: redirige vers la page désirée	
+                item.on("click", function(e){
+                    //var fieldbar = Y.FieldBar.getStates(this);
+                    var page_id = this.get('text');
+                    window.open(wfw.Navigator.getURI(page_id),"_self");
+                });
+            },
+            onCreateBar: function(contener){
+                //insert un titre à la barre principale
+                var text_label = Y.Node.create('<label>');
+                text_label.addClass('fieldbar_fil_ariane_item_title');
+                text_label.insert("Navigation");
+                contener.insert(text_label);
+            }
+        });
+    };
+    
+    Y.one('window').on('load', onLoad);
+});
+
