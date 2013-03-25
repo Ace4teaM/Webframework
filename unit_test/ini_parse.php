1c3063e6ffc5c641a0a58fd61dd4489e0a2eed5d
Thomas AUGUEY <contact@aceteam.org>
2013-03-25 17:31:33 +0100

    update unit test

--- unit_test/ini_parse.php
+++ unit_test/ini_parse.php
@@ -2,4 +2,18 @@
 require_once("inc/globals.php");
 global $app;
 
+$data = parse_ini_file_ex("bin/types.ini");
+print_r($data);
+
+//keyword->value5
+if(is_null($data["KEYWORD"]["VALUE5"]) )
+    echo("OK");
+else
+    echo("KO");
+
+if($data["STRING"]["VALUE1"] === "false" )
+    echo("OK");
+else
+    echo("KO");
+
 ?>
