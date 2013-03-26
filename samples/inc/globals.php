a041199b60bb7c6dc5c3349b2ec7761b83931947
Thomas AUGUEY <contact@aceteam.org>
2013-03-26 12:43:16 +0100

    update entry point

--- samples/inc/globals.php
+++ samples/inc/globals.php
@@ -1,23 +1,32 @@
 <?php
 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 define("THIS_PATH", dirname(__FILE__)); //chemin absolue vers ce script
 define("ROOT_PATH", realpath(THIS_PATH."/../")); //racine du site
 
-//charge la configuration
-$config = parse_ini_file(ROOT_PATH."/cfg/config.ini", true);
-$config = array_change_key_case($config, CASE_UPPER);
-if(!isset($config["PATH"])){
-    echo("Cant't find configuration path section");
+//recupere la constante du chemin d'accès vers wfw
+//indispensable pour parser la configuration puis initialiser l'application
+$ini_file_content = file_get_contents(ROOT_PATH."/cfg/config.ini");
+if(!preg_match('/(?:^|[\n\r\s]+)@const\s+wfw_path\s*=\s*\"([^\"]*)/', $ini_file_content, $matches)){
+    echo("Can't find Webframework Path constant in configuration file");
     exit(-1);
 }
-$config["PATH"] = array_change_key_case($config["PATH"], CASE_UPPER);
-if(!isset($config["PATH"]["WFW"])||!isset($config["PATH"]["WFW_LOCAL"])){
+$wfw_path = trim($matches[1]);
+
+//ajoute le chemin d'accès à WFW
+set_include_path(get_include_path() . PATH_SEPARATOR . $wfw_path);
+
+//fonction de parsing (fichier ini)
+include('php/ini_parse.php');
+
+//charge la configuration
+$config = parse_ini_string_ex($ini_file_content);
+if(!isset($config["PATH"]) || !isset($config["PATH"]["WFW"]) || !isset($config["PATH"]["WFW_LOCAL"])){
     echo("Cant't find Webframework Path");
     exit(-1);
 }
-
-//ajoute le chemin d'accès à WFW
-set_include_path(get_include_path() . PATH_SEPARATOR . $config["PATH"]["WFW_LOCAL"]);
 
 
 //instancie l'application
