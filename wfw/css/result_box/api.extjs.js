4a2af8daf338991bd2120bf68721cb078bd63582
Thomas AUGUEY <contact@aceteam.org>
2013-03-28 12:45:41 +0100

    add support for extjs application

--- wfw/css/result_box/api.extjs.js
+++ wfw/css/result_box/api.extjs.js
@@ -1,4 +1,4 @@
-
+﻿
 /*------------------------------------------------------------------------------------------------------------------*/
 /**
  * @brief Initialise la vue de résultat
@@ -12,26 +12,4 @@ MyApp.showResultToElement = function(node,result)
     node.one("> div").set("className",result.result);
     node.one(".err").set("text",result.getError());
     node.one(".msg").set("text",result.getMessage());
-}
-
-MyApp.showResultToMsg = function(result)
-{
-    var obj = {
-        title: result.getResult(),
-        msg: result.getError(),
-        buttons: Ext.Msg.OK,
-        icon: Ext.Msg.QUESTION
-   };
-   var msg = result.getMessage();
-   if(msg)
-       obj.msg += "<br/>"+result.getMessage();
-   switch(result.result){
-       case "ERR_FAILED":
-           obj.icon = Ext.Msg.WARNING;
-           break;
-       case "ERR_SYSTEM":
-           obj.icon = Ext.Msg.ERROR;
-           break;
-   }
-    Ext.Msg.show(obj);
 }
