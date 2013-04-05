c8101902419b64603db0bb62facfe018e371ebf4
Thomas AUGUEY <contact@aceteam.org>
2013-04-05 16:20:42 +0200

    add random_token to SQL functions

--- sql/postgresql/func.sql
+++ sql/postgresql/func.sql
@@ -187,3 +187,29 @@ begin
 end;
 $$
 LANGUAGE plpgsql;
+
+/*
+  Génère un token aléatoire
+    @param int token_length Taille du token
+  Retourne:
+     [TEXT] Token généré
+
+*/
+
+CREATE OR REPLACE FUNCTION random_token(token_length int)
+RETURNS text AS 
+$$
+DECLARE
+  chars text[] := '{0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z}';
+  result text := '';
+  i integer := 0;
+  chars_length integer := 62; /*array_length(chars, 1); PG-8.4! */
+BEGIN
+  for i in 1..token_length loop
+    result := result || chars[round(random()*(chars_length-1))];
+  end loop;
+  return result;
+END;
+$$
+LANGUAGE plpgsql;
+
