5268b08d98d74f1203cfa1f37ad6ae6b17108ae8
Thomas AUGUEY <contact@aceteam.org>
2013-04-11 17:17:35 +0200

    fix factor field format

--- wfw/php/inputs/factor.php
+++ wfw/php/inputs/factor.php
@@ -10,21 +10,20 @@ require_once("$libdir/php/class/bases/input.php");
 class cInputFactor extends cInput
 {
 	public static function isValid($value){
-		//if( empty_string($value) )
-		//	return ERR_TEXT_EMPTY;
-		if(!preg_match('/(0|1)(\.[0-9]*)?/', $value))
-			return RESULT(cResult::Failed,cInput::InvalidChar);
+            // chaine valide ?
+            if(preg_match("/^".cInputFloat::regExp()."$/",$value)==0)
+                    return RESULT(cResult::Failed,cInput::InvalidChar);
 
-		return RESULT_OK();
+            return RESULT_OK();
 	}     
         public static function toObject($string) {
             return floatval($string);
         }
 	public static function regExp(){
-		return '(0|1)(\\.[0-9]*)?';
+            return '(0|1|0\\.[0-9]*)?';
 	}
 	public static function getMaxLength(){
-		return 128;
+            return 128;
 	}
 }
 
