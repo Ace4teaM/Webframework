5a0eb92a7f25c2aa1f9573cee1e87d81b83cf203
Thomas AUGUEY <contact@aceteam.org>
2013-04-23 17:51:55 +0200

    fix Float type
    non negative value

---
+++ wfw/javascript/types/float.js
@@ -0,0 +1,53 @@
+/*
+    ---------------------------------------------------------------------------------------------------------------------------------------
+    (C)2013 Thomas AUGUEY <contact@aceteam.org>
+    ---------------------------------------------------------------------------------------------------------------------------------------
+    This file is part of WebFrameWork.
+
+    WebFrameWork is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    WebFrameWork is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with WebFrameWork.  If not, see <http://www.gnu.org/licenses/>.
+    ---------------------------------------------------------------------------------------------------------------------------------------
+*/
+
+
+var cInputFactor={
+    isValid : function(value){
+        if (empty_string(value))
+            return RESULT(cResult.Failed, cInput.EmptyText);
+
+        //test le format
+        var regex = '^'+this.regExp()+'$';
+        if ( !(new RegExp(regex)).test(value) )
+            return RESULT(cResult.Failed, cInput.InvalidChar);
+        
+        return RESULT_OK();
+    }
+    ,
+    regExp : function(){
+        return '(?:0|1)(?:\\.[0-9]+)?';
+    }
+    ,
+    getMaxLength : function(){
+        return 128;
+    }
+    ,
+    getMinLength : function(){
+        return 1;
+    }
+
+    ,
+    toObject : function(value){
+        return parseFloat(value);
+    }
+}
+cInputfactor = cInputFactor; //global insensitive scope
