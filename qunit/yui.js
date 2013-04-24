e0b7f0b226ce7ce96607e729324768c1cc746fba
Thomas AUGUEY <contact@aceteam.org>
2013-04-24 12:11:29 +0200

    Update YUI unit test
    wfw.URI

--- qunit/yui.js
+++ qunit/yui.js
@@ -1,5 +1,5 @@
 
-YUI(wfw_yui_config('../wfw/javascript/yui/')).use('wfw', 'wfw-form', 'wfw-path',function(Y){
+YUI(wfw_yui_config('../wfw/javascript/yui/')).use('wfw', 'wfw-form', 'wfw-path', 'wfw-uri',function(Y){
     var wfw = Y.namespace("wfw");
 
     /* wfw */
@@ -15,7 +15,7 @@ YUI(wfw_yui_config('../wfw/javascript/yui/')).use('wfw', 'wfw-form', 'wfw-path',
     });
     
     
-    /* wfw-form */
+    /* wfw.Form */
     module( "wfw.Form" );
     test( "Form" , function() {
         //obtient les valeurs
@@ -41,7 +41,9 @@ YUI(wfw_yui_config('../wfw/javascript/yui/')).use('wfw', 'wfw-form', 'wfw-path',
         ok( elements.fooinput, "Get Elements By Id (lower case)" );
     });
     
-    /* wfw-form */
+    
+    
+    /* wfw.Path */
     module( "wfw.Path" );
     test( "Path" , function() {
         //nom de fichier
@@ -49,5 +51,51 @@ YUI(wfw_yui_config('../wfw/javascript/yui/')).use('wfw', 'wfw-form', 'wfw-path',
         equal( wfw.Path.filename('yui.html'), 'yui', "Get Filename without path" );
         equal( wfw.Path.filename('yui'), 'yui', "Get Filename without extension" );
         equal( wfw.Path.filename('yui.html',{include_ext:true}), 'yui.html', "Get Filename include extension" );
+    });
+    
+    
+    
+    /* wfw.URI */
+    module( "wfw.URI" );
+    test( "URI" , function() {
+        //Cut
+        var uri_str = 'http://www.aceteam.org/Webframework/qunit/yui.html?a=b#hello';
+        var uri = wfw.URI.cut(uri_str);
+        equal( uri.addr     , uri_str, "Address" );
+        equal( uri.scheme   , 'http', "Scheme" );
+        equal( uri.authority, 'www.aceteam.org', "Authority" );
+        equal( uri.path     , 'Webframework/qunit/yui.html', "Path" );
+        equal( uri.query    , 'a=b', "Query" );
+        equal( uri.fragment , 'hello', "Fragment" );
+        equal( uri.makeAddress() , uri_str, "Make Address" );
+        
+        //Decode
+        var uri_str = 'Hello%20World';
+        equal( wfw.URI.decode(uri_str), 'Hello World', "Decode" );
+        var uri_str = 'Hello%CE%A3World';
+        equal( wfw.URI.decodeUTF8(uri_str), 'HelloΣWorld', "Decode UTF-8" );
+        
+        //Encode
+        var uri_str = 'Hello World';
+        equal( wfw.URI.encode(uri_str), 'Hello%20World', "Encode" );
+        var uri_str = 'HelloΣWorld';
+        equal( wfw.URI.encodeUTF8(uri_str), 'Hello%CE%A3World', "Encode UTF8" );
+        
+        //Make
+        equal( wfw.URI.make('http','aceteam.org','index.html','foo=bar','test'), 'http://aceteam.org/index.html?foo=bar#test', "Make" );
+        
+        //Query
+        deepEqual( wfw.URI.query_to_object("voiture=alpha&lieu=paris"), {voiture:"alpha",lieu:"paris"}, "Query to object");
+        deepEqual( wfw.URI.query_to_object("voiture=alpha%20romeo", true), {voiture:"alpha romeo"}, "Query to object (encoded)");
+        
+        //Query
+        equal( wfw.URI.object_to_query({voiture:"alpha",lieu:"paris"}), "voiture=alpha&lieu=paris", "Object to query");
+        equal( wfw.URI.object_to_query({voiture:"alpha romeo"}, true), "voiture=alpha%20romeo", "Object to query (encoded)");
+        
+        //Remake
+        equal( wfw.URI.remakeURI('http://www.aceteam.org?foo=bar',{foo:"foo",bar:"bar"}), "http://www.aceteam.org?foo=foo&bar=bar", "Remake URI");
+        equal( wfw.URI.remakeURI('http://www.aceteam.org?foo=bar',{bar:"bar"},wfw.URI.ReplaceQuery,'test'), "http://www.aceteam.org?bar=bar#test", "Remake URI (replace)");
+        equal( wfw.URI.remakeURI('http://www.aceteam.org?foo=bar',null,wfw.URI.ReplaceQuery,'test'), "http://www.aceteam.org#test", "Remake URI (anchor)");
+        
     });
 });
