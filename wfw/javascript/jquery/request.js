2160039646102e67cdd632fb0aac34726dc0adb9
Thomas AUGUEY <contact@aceteam.org>
2013-09-18 17:04:35 +0200

    update request plugin for JQuery

--- wfw/javascript/jquery/request.js
+++ wfw/javascript/jquery/request.js
@@ -20,31 +20,121 @@
 */
 
 /**
- * @brief jQuery Form Plugin
- * @method form
+ * @brief jQuery Request Plugin
+ * @method request
  * @memberof JQuery
  * 
  * #Introduction
- * Crée un formulaire basé sur le model de données
+ * Simplifie la gestion des requêtes HTTP dans une application Webframework
  * 
- * ## Définit les valeurs de champs
- * form( 'values', values[] )
+ * ## Execute une requête XARG
+ * request( 'xarg', 'my_page_id', { request params... }, function(obj,args){ 
+ *      obj;   // request object
+ *      args;  // the native javascript object of response parameters
+ * } )
+ *
+ * ## Execute une requête XARG (avec paramètres)
+ * request( 'xarg', 'my_page_id', { args... }, { 
+ *     no_result : false,                // Si spécifié, le contenu du fichier est retourné sans traitement des erreurs
+ *     no_msg    : true,                 // Si spécifié, les messages d'erreurs ne sont pas affichés à l'écran
+ *     onsuccess : function(obj,args){}, // Optionnel, callback en cas de succès
+ *     onfailed  : function(obj,args){}, // Optionnel, callback en cas de échec
+ *     onerror   : function(obj){},      // Optionnel, callback en cas d'erreur de transmition de la requête
+ *     wfw_form_name : "formName"        // Optionnel, nom associé à l'élément FORM recevant les données de retours
+ * } )
  * 
- * ## Obtient les valeurs de champs
- * values[] form( 'values' )
+ * ## Execute une requête XML
+ * request( 'xml', 'my_page_id', { request params... }, function(obj,xml_doc, xml_root){ 
+ *      obj;      // request object
+ *      xml_doc;  // the native XMLDOMDocuemnt interface
+ *      xml_root; // the root document element
+ * } )
+ *
+ * ## Execute une requête XML (avec paramètres)
+ * request( 'xml', 'my_page_id', { args... }, { 
+ *     no_result : false,                // Si spécifié, le contenu du fichier est retourné sans traitement des erreurs
+ *     no_msg    : true,                 // Si spécifié, les messages d'erreurs ne sont pas affichés à l'écran
+ *     onsuccess : function(obj,xd,xr){},// Optionnel, callback en cas de succès
+ *     onfailed  : function(obj,xd,xr){},// Optionnel, callback en cas de échec
+ *     onerror   : function(obj){},      // Optionnel, callback en cas d'erreur de transmition de la requête
+ *     wfw_form_name : "formName"        // Optionnel, nom associé à l'élément FORM recevant les données de retours
+ * } )
  * 
- * ## Initialise le formulaire
- * form( )
- * 
- * ## Initialise le formulaire avec des champs définits
- * form( fields:[ {name,value,type}, ... ] )
- * 
- * ## Transmet le formulaire 
- * form( fields:[ {name,value,type}, ... ] )
-
- * **/
+ * ## Execute une requête standard
+ * request( 'add', { 
+ *      name              : null,   // Nom symbolique de la requête
+ *      url               : "",     // URL de destination d ela requête
+ *      args              : null,   // Arguments passés en paramétre de la fonction
+ *      response_header   : null,   // En-têtes de la réponse
+ *      response          : null,   // Corps de la reponse texte
+ *      response_obj      : null,   // Objet natif de la requete (XHR)
+ *      callback          : function(req_obj,user){},// Optionnel, callback d'avancement de la request
+ *      user              : {},     // Paramétres "utilisateur" passé en argument de callback
+ *      status            : "wait", // Statut actuel de la requête ("wait" ou code réponse HTTP ex:200)
+ *      remove_after_exec : true,   // Supprime la requête de la liste après exécution
+ *      async             : true    // Asynchrone/Synchrone
+ * } )
+ *
+ **/
 (function($)
 {
+    //s'assure que le plugin navigator est initialisé
+    $(window).navigator();
+
+    /*
+    * Ajoute une requête HTTP
+    * @param args Objet de requete
+    */
+    function add(args){
+        var req_obj = object_merge({
+            name              : null,   // Nom symbolique de la requête
+            url               : "",     // URL de destination d ela requête
+            args              : null,   // Arguments passés en paramétre de la fonction
+            response_header   : null,   // En-têtes de la réponse
+            response          : null,   // Corps de la reponse texte
+            response_obj      : null,   // Objet natif de la requete (XHR)
+            callback          : function(req_obj,user){},// Optionnel, callback d'avancement de la request
+            user              : {},     // Paramétres "utilisateur" passé en argument de callback
+            status            : "wait", // Statut actuel de la requête ("wait" ou code réponse HTTP ex:200)
+            remove_after_exec : true,   // Supprime la requête de la liste après exécution
+            async             : true    // Asynchrone/Synchrone
+        },args);
+
+        // genere le nom ?
+        if ((typeof (req_obj.name) != 'string') || empty(req_obj.name))
+            req_obj.name = uniqid();
+        //transforme les arguments
+        if(typeof (req_obj.args) == "string")
+            req_obj.args = uri_query_to_object(req_obj.args);
+
+        $.ajax({
+            type:"POST",
+            url: req_obj.url,
+            data: req_obj.args,
+            /*context: req_obj,*/
+            async:req_obj.async,
+            success: function( /*PlainObject*/ data, /*String*/ textStatus, /*jqXHR*/ jqXHR ) {
+                req_obj.status = jqXHR.status; //met a jour l'etat
+                req_obj.response_header = jqXHR.getAllResponseHeaders();
+                req_obj.response = data;
+                req_obj.response_obj = jqXHR;
+                if(typeof req_obj.callback == "function")
+                    req_obj.callback(req_obj,req_obj.user);
+            },
+            error: function(/*jqXHR*/ jqXHR, /*String*/ textStatus, /*String*/ errorThrown) {
+                req_obj.status = jqXHR.status; //met a jour l'etat
+                req_obj.response_header = jqXHR.getAllResponseHeaders();
+                req_obj.response = null;
+                req_obj.response_obj = jqXHR;
+                if(typeof req_obj.callback == "function")
+                    req_obj.callback(req_obj,req_obj.user);
+            }
+        }); 
+
+        return req_obj;
+    };
+
+
     //constructeur
     $.fn.request = function(p){
         var me = $(this);
@@ -59,79 +149,139 @@
                 case "xarg":
                     var req_page      = args[1];
                     var req_args      = args[2];
-                    var req_onsuccess = args[3]; // si null sync
-                    var req_onfailed  = args[4];
+                    var req_params    = args[3]; // si null sync
 
-                    //sync request
-                    return me.request("add",{
-                        url: $.navigator("page",req_page),
+                    var req_obj = {
+                        url: $(window).navigator("page",req_page),
                         args: req_args,
-                        callback: function(reqObj){
-                            if(reqObj.status == "success"){
-                                expected = "...";
-                                value = reqObj.response;
-                                equal( value , expected, "sync request" );
-                            }
-                            if(reqObj.status == "error"){
-                                expected = "...";
-                                value = reqObj.response;
-                                equal( value , expected, "sync request" );
+                        async:false,
+                        type:"text/plain",
+                        user: ((typeof req_params == "function") ? { onsuccess : req_params } : req_params),
+                        callback: function(obj){
+                            switch(obj.status){
+                                //OK
+                                case 200:
+                                    //callback params
+                                    var param = object_merge({
+                                       no_result : false,                // Si spécifié, le contenu du fichier est retourné sans traitement des erreurs
+                                       no_msg    : true,                 // Si spécifié, les messages d'erreurs ne sont pas affichés à l'écran
+                                       onsuccess : function(obj,args){}, // Optionnel, callback en cas de succès
+                                       onfailed  : function(obj,args){}, // Optionnel, callback en cas de échec
+                                       onerror   : function(obj){},      // Optionnel, callback en cas d'erreur de transmition de la requête
+                                       wfw_form_name : "formName"        // Optionnel, nom associé à l'élément FORM recevant les données de retours
+                                    },obj.user);
+                                    
+                                    //resultat ?
+                                    var args = xarg_to_object(obj.response,false);
+                                    if (!args) {
+                                        if (!param.no_msg)
+                                            console.log("Erreur de requête", obj.response);
+                                        param.onerror(obj);
+                                        break;
+                                    }
+
+                                    //non x-argument result !
+                                    if ((!param.no_result) && typeof(args.result) == 'undefined') {
+                                        if (!param.no_msg)
+                                            console.log("Résultat de requête non spécifié", obj.response);
+                                        param.onerror(obj);
+                                        break;
+                                    }
+
+                                    //erreur ?
+                                    if ((!param.no_result) && (args.result != "ERR_OK")) {
+                                        //failed callback
+                                        param.onfailed(obj, args);
+                                        break;
+                                    }
+
+                                    //success callback
+                                    param.onsuccess(obj, args);
+                                    break;
+                                default:
+                                    console.log("xarg other");
+                                    break;
                             }
                         }
-                    });
+                    };
+
+                    //sync request
+                    return add(req_obj);
+
+                /**
+                 * @param opt Objet de requete
+                 */
+                case "xml":
+                    var req_page      = args[1];
+                    var req_args      = args[2];
+                    var req_params    = args[3];
+
+                    var req_obj = {
+                        url: $(window).navigator("page",req_page),
+                        args: req_args,
+                        async:false,
+                        user:req_params,
+                        callback: function(obj){
+                            switch(obj.status){
+                                //OK
+                                case 200:
+                                    //callback params
+                                    var param = object_merge({
+                                       no_result : false,                // Si spécifié, le contenu du fichier est retourné sans traitement des erreurs
+                                       no_msg    : true,                 // Si spécifié, les messages d'erreurs ne sont pas affichés à l'écran
+                                       onsuccess : function(obj,xd,xr){},// Optionnel, callback en cas de succès
+                                       onfailed  : function(obj,xd,xr){},// Optionnel, callback en cas de échec
+                                       onerror   : function(obj){},      // Optionnel, callback en cas d'erreur de transmition de la requête
+                                       wfw_form_name : "formName"        // Optionnel, nom associé à l'élément FORM recevant les données de retours
+                                    },obj.user);
+                                    
+                                    //resultat ?
+                                    var xml_doc = xml_parse(obj.response);
+                                    if (xml_doc == null) {
+                                        console.log("Document XML mal formé", obj.response);
+                                        param.onerror(obj);
+                                        break;
+                                    }
+                                    var xml_root = $(xml_doc.documentElement);
+
+                                    //callback
+                                    if (!param.no_result) {
+                                        var result = $("result",xml_root);
+                                        if (result != null) {
+                                            //echec ?
+                                            if (result.text() != "ERR_OK") {
+                                                // failed callback
+                                                param.onfailed(obj, xml_doc, xml_root);
+                                                break;
+                                            }
+                                        }
+                                        else{
+                                            if (!param.no_msg)
+                                                console.log("Résultat de requête non spécifié", obj.response);
+                                            param.onerror(obj, xml_doc, xml_root);
+                                        }
+                                    }
+                                    param.onsuccess(obj, xml_doc, xml_root);
+                                    break;
+                                default:
+                                    console.log("xarg other");
+                                    break;
+                            }
+                        }
+                    };
+
+                    //sync request
+                    return add(req_obj);
+
                 /**
                  * @param opt Objet de requete
                  */
                 case "add":
-                    var req_obj = object_merge({
-                        name              : null,
-                        url               : "",
-                        args              : null,
-                        response_header   : null,
-                        response          : null,
-                        response_obj      : null,
-                        callback          : null,
-                        user              : {},
-                        status            : "wait",
-                        remove_after_exec : true,
-                        async             : true
-                    },args[1]);
-
-                    // genere le nom ?
-                    if ((typeof (req_obj.name) != 'string') || empty(req_obj.name))
-                        req_obj.name = uniqid();
-                    //transforme les arguments
-                    if(typeof (req_obj.args) == "string")
-                        req_obj.args = uri_query_to_object(req_obj.args);
-
-                    $.ajax({
-                        type:"POST",
-                        url: req_obj.url,
-                        data: req_obj.args,
-                        /*context: req_obj,*/
-                        async:req_obj.async,
-                        success: function( /*PlainObject*/ data, /*String*/ textStatus, /*jqXHR*/ jqXHR ) {
-                            console.log(textStatus);
-                            req_obj.status = textStatus; //met a jour l'etat
-                            req_obj.response_header = jqXHR.getAllResponseHeaders();
-                            req_obj.response = data;
-                            req_obj.response_obj = data;
-                            req_obj.callback(req_obj);
-                        },
-                        error: function(/*jqXHR*/ jqXHR, /*String*/ textStatus, /*String*/ errorThrown) {
-                            req_obj.status = textStatus; //met a jour l'etat
-                            req_obj.response_header = jqXHR.getAllResponseHeaders();
-                            req_obj.response = null;
-                            req_obj.response_obj = null;
-                            req_obj.callback(req_obj);
-                        }
-                    }); 
-
+                    var req_obj = add(args[1]);
                     return this;
             }
         }
             
        return this;
     };
-    
 })(jQuery);
